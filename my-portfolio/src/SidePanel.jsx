819b51ed2161b65b3df87c2f1b03813d4d862971
zaki <zakip1r1o13@gmail.com>
2025-01-17 01:26:38 +0100

    ignore last commit

--- my-portfolio/src/SidePanel.jsx
+++ my-portfolio/src/SidePanel.jsx
@@ -15,17 +15,17 @@ function SidePanel(){
         <div><ul className="@[360px]:space-y-2">
         <li>
     
-          <Link to="/About" className="font-normal @[360px]:mt-2  mt-[-5%] ml-6 @[200px]:text-[18px] @[240px]:text-[20px] @[277px]:text-2xl @[330px]:text-3xl inline-block m-2 rounded hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
+          <Link to="/About" className="font-normal @[360px]:mt-2  mt-[-5%] ml-6 text-[16px] @[250px]:text-[20px] @[277px]:text-2xl @[330px]:text-3xl inline-block m-2 rounded hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
             About
           </Link>
         </li>
         <li>
-          <Link to="/Ideas" className="font-normal @[360px]:mt-2  mt-[-6%] mt-0.5 ml-6 @[200px]:text-[18px] @[240px]:text-[20px] @[277px]:text-2xl @[330px]:text-3xl  inline-block m-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
+          <Link to="/Ideas" className="font-normal @[360px]:mt-2  mt-[-6%] mt-0.5 ml-6 text-[16px] @[250px]:text-[20px] @[277px]:text-2xl @[330px]:text-3xl  inline-block m-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
             Ideas
           </Link>
         </li>
         <li>
-          <a href="/CV.pdf" download className="font-normal mb-[8px] @[360px]:mb-10 @[360px]:mt-2  mt-[-5%] ml-6 @[200px]:text-[18px] @[240px]:text-[20px] @[277px]:text-2xl @[330px]:text-3xl  inline-block m-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
+          <a href="/CV.pdf" download className="font-normal mb-[8px] @[360px]:mb-10 @[360px]:mt-2  mt-[-5%] ml-6 text-[16px] @[250px]:text-[20px] @[277px]:text-2xl @[330px]:text-3xl  inline-block m-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
             CV
           </a>
         </li>
@@ -34,20 +34,20 @@ function SidePanel(){
 
 
       <div>
-        <h3 className="opacity-75 @[200px]:text-[18px] @[240px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl  tracking-widest font-normal mb-0 @[360px]:mb-4 pt-4 pl-6">MILESTONES</h3>
+        <h3 className="opacity-75 text-[16px] @[250px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl  tracking-widest font-normal mb-0 @[360px]:mb-4 pt-4 pl-6">MILESTONES</h3>
         <ul className="">
         <li>
-          <Link to="/Projects" className=" font-normal pt-2 pl-6 @[200px]:text-[18px] @[240px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl inline-block p-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
+          <Link to="/Projects" className=" font-normal pt-2 pl-6 text-[16px] @[250px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl inline-block p-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
             Projects
           </Link>
         </li>
         <li>
-          <Link to="/Skills" className="font-normal pt-0.5 pl-6 @[200px]:text-[18px] @[240px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl inline-block p-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
+          <Link to="/Skills" className="font-normal pt-0.5 pl-6 text-[16px] @[250px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl inline-block p-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
             Skills
           </Link>
         </li>
         <li>
-          <Link to="/Goals" className="font-normal  @[360px]:mb-24 mb-4 pt-0.5 pl-6 @[200px]:text-[18px] @[240px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl inline-block p-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out ">
+          <Link to="/Goals" className="font-normal  @[360px]:mb-24 mb-4 pt-0.5 pl-6 text-[16px] @[250px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl inline-block p-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out ">
             Goals
           </Link>
         </li>
@@ -57,30 +57,30 @@ function SidePanel(){
 
 
       <div>
-      <h3 className="opacity-75 @[200px]:text-[18px] @[240px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl tracking-widest font-normal mb-2 pt-6 pl-6">CONTACT</h3>
+      <h3 className="opacity-75 text-[16px] @[250px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl tracking-widest font-normal mb-2 pt-6 pl-6">CONTACT</h3>
         <ul>
         <li>
-          <Link to="#" onClick={handleMailClick} className="mt-1 font-normal pt-2 pl-6 @[200px]:text-[18px] @[240px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl inline-block p-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
+          <Link to="#" onClick={handleMailClick} className="mt-1 font-normal pt-2 pl-6 text-[16px] @[250px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl inline-block p-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
             Mail
           </Link>
         </li>
         <li>
-          <Link to="https://www.instagram.com/zakitooshort/profilecard/?igsh=MndjbHM5MzJ5c3Ft" target="_blank" className="font-normal pt-0.5 pl-6 @[200px]:text-[18px] @[240px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl inline-block p-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
+          <Link to="https://www.instagram.com/zakitooshort/profilecard/?igsh=MndjbHM5MzJ5c3Ft" target="_blank" className="font-normal pt-0.5 pl-6 text-[16px] @[250px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl inline-block p-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
             Instagram
           </Link>
         </li>
         <li>
-          <Link to="https://github.com/zakitooshort" target="_blank" className="font-normal pt-0.5 pl-6 @[200px]:text-[18px] @[240px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl inline-block p-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
+          <Link to="https://github.com/zakitooshort" target="_blank" className="font-normal pt-0.5 pl-6 text-[16px] @[250px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl inline-block p-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
             Github
           </Link>
         </li>
         <li>
-          <Link to="https://read.cv/zakitooshort" target="_blank" className="font-normal pt-0.5 pl-6 @[200px]:text-[18px] @[240px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl inline-block p-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
+          <Link to="https://read.cv/zakitooshort" target="_blank" className="font-normal pt-0.5 pl-6 text-[16px] @[250px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl inline-block p-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
             Read.cv
           </Link>
         </li>
         <li>
-          <Link to="https://leetcode.com/u/zakitooshort/" target="_blank" className="font-normal pt-0.5 pl-6 @[200px]:text-[18px] @[240px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl inline-block p-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
+          <Link to="https://leetcode.com/u/zakitooshort/" target="_blank" className="font-normal pt-0.5 pl-6 text-[16px] @[250px]:text-[20px] @[277px]:text-1xl @[330px]:text-2xl inline-block p-2 rounded  hover:text-[#B6B6B4] hover:underline transition duration-200 ease-in-out">
             Leetcode
           </Link>
         </li>
